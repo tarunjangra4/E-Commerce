2ed7a0d37b40ff8657fce369d29e2d28e20c489c
Tarun Jangra <51834686+tarunjangra4@users.noreply.github.com>
2023-08-09 12:15:28 +0530

    e commerce website complete code

--- src/App.js
+++ src/App.js
@@ -40,15 +40,15 @@ function App() {
         <Routes>
           <Route index path="/" element={<Home />} />
 
-          {/* <Route path="categories" element={<Categories />}>
-          <Route path="all" element={<All />} />
-          <Route path="furnitures" element={<Furnitures />} />
-          <Route path="electronics" element={<Electronics />} />
-          <Route path="lamps" element={<Lamps />} />
-          <Route path="kitchen" element={<Kitchen />} />
-          <Route path="chairs" element={<Chairs />} />
-          <Route path="skin-care" element={<SkinCare />} />
-        </Route> */}
+          <Route path="categories" element={<Categories />}>
+            <Route path="all" element={<All />} />
+            <Route path="furnitures" element={<Furnitures />} />
+            <Route path="electronics" element={<Electronics />} />
+            <Route path="lamps" element={<Lamps />} />
+            <Route path="kitchen" element={<Kitchen />} />
+            <Route path="chairs" element={<Chairs />} />
+            <Route path="skin-care" element={<SkinCare />} />
+          </Route>
           <Route path="categories/product/:id" element={<ProductPage />} />
         </Routes>
       </CartContext.Provider>
